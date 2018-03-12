f7958b8a00c364c7daa36ee3b5653e15a5c81c60
宋鹏 <songpeng02@meituan.com>
2018-03-12 10:48:03 +0800

    feature-delete update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,18 +8,19 @@ export enum BranchTypes {
 }
 
 export interface BaseConfig {
-  [index: string]: string
+  [index: string]: string;
 }
 
 export interface BMAConfig {
-  [index: string]: any,
-  enforcePRtoMaster?: boolean,
-  branch: BaseConfig,
-  branchPrefix: BaseConfig
+  [index: string]: any;
+  enforcePRtoMaster?: boolean;
+  branch: BaseConfig;
+  branchPrefix: BaseConfig;
 }
 
 export function newBranch(type: BranchTypes, id: number | string, config: {silence?: boolean}): Promise <boolean | object>
 export function mergeMasterToBranch(branchName: string, config: {silence?: boolean}): Promise <boolean | object>
 export function mergeToMainBranch(branchName: string, mainBranch: string, config: {mergeMaster: boolean, forceEnvBranch?: string, silence?: boolean}): Promise <boolean | object>
+export function deleteAndUpdateMaster(branchName: string, config: {silence?: boolean}) : Promise <boolean>
 export function setConfig(key: string, newConfig: BaseConfig): void
 export function setEnforcePRtoMaster(state: boolean): void
